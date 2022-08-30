9549258e89e9d3e2923c377921455f6168d25a90
Eduardo Hejazi Bandeira <106497109+eduhejband@users.noreply.github.com>
2022-08-30 18:05:44 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+/* Menu responsivo - https://www.youtube.com/watch?v=bHRXRYTppHM */
+
+class MobileNavbar{
+  constructor(mobileMenu, navList, navLinks){
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+    
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick(){
+    this.navList.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent(){
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu){
+      this.addClickEvent(this.mobileMenu);
+    }
+    return this;
+  }
+  }
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
+
+
+
+/* Sticky header parar tornar o cabeçalho fixo - https://www.w3schools.com/howto/howto_js_sticky_header.asp */
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}

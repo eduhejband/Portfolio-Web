7d5e966e1f4c727371c7ac0de04d72342c30bbc8
eduhejband <eduardohejband@gmail.com>
2022-10-09 11:21:46 -0300

    update

--- script.js
+++ script.js
@@ -62,32 +62,30 @@ function myFunction() {
   }
 }
 
-// Get the modal
+
+
+// Janela para copiar o email
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
+// "Copy select" da janela
 function myFunction() {
   var copyText = document.getElementById("myInput");
   copyText.select();

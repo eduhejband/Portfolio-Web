cee68204cb5a87b6ff5798535efb5f6a6849bec5
Eduardo Hejazi Bandeira <106497109+eduhejband@users.noreply.github.com>
2022-12-14 20:29:56 -0300

    Update

--- script.js
+++ script.js
@@ -279,3 +279,33 @@ var accordion = (function() {
         
     } accordionInner(0);
 })();
+
+
+
+// Efeito de paginação - site de modelo: https://www.rhuanbello.com/
+const initAnimationScroll = () => {
+  const sections = document.querySelectorAll('.effect')
+
+  const windowHalfSize = window.innerHeight * 0.6
+  
+  const animateScroll = () => {
+      sections.forEach(item => {
+          const sectionTop = item.getBoundingClientRect().top
+
+          const isSectionVisible = (sectionTop - windowHalfSize) < 0
+
+          if (isSectionVisible) {
+              item.classList.add('active')
+          } else {
+              item.classList.remove('active')
+          }
+
+      }) 
+  }
+
+  animateScroll()
+
+  window.addEventListener('scroll', animateScroll)
+}
+
+initAnimationScroll()
